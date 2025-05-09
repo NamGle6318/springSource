ed2d2733cb4b49302de309db8ea8bb2ca42e52d1
HyunWoo <mogs223@gmail.com>
2025-05-09 14:32:03 +0900

    board 완성 security 적용전

--- board/src/main/resources/static/js/read.js
+++ board/src/main/resources/static/js/read.js
@@ -8,61 +8,106 @@
 
 // date
 
+// replyForm
+const replyForm = document.querySelector(".replyForm");
 // 댓글창 출력
-const replyList = (data) => {
-  let result = "";
+const replyList = () => {
+  axios.get(`/replies/board/${bno}`).then((res) => {
+    let data = res.data;
 
-  // 댓글창에 출력할 데이터 형식 지정
-  const formatDate = (str) => {
-    const date = new Date(str);
-    console.log();
-    return (
-      date.getFullYear() +
-      "/" +
-      (date.getMonth() + 1) +
-      "/" +
-      date.getDate() +
-      " " +
-      date.getHours() +
-      ":" +
-      date.getMinutes()
-    );
-  };
+    let result = "";
 
-  data.forEach((reply) => {
-    // let date = formatDate(reply.createdDate);
-    result += ` <div class="d-flex justify-content-between my-2 border-bottom reply-row" data-rno="${reply.rno}">`;
-    result += `<div class="p-3">`;
-    result += `<img
+    // 댓글창에 출력할 데이터 형식 지정
+    const formatDate = (str) => {
+      const date = new Date(str);
+      console.log();
+      return (
+        date.getFullYear() +
+        "/" +
+        (date.getMonth() + 1) +
+        "/" +
+        date.getDate() +
+        " " +
+        date.getHours() +
+        ":" +
+        date.getMinutes()
+      );
+    };
+
+    data.forEach((reply) => {
+      result += ` <div class="d-flex justify-content-between my-2 border-bottom reply-row" data-rno="${reply.rno}">`;
+      result += `<div class="p-3">`;
+      result += `<img
                 src="/img/default.png"
                 alt=""
                 class="rounded-circle mx-auto d-block"
                 style="width: 60px; height: 60px"
               />`;
-    result += `</div>`;
-    result += `<div class="flex-grow-1 align-self-center">`;
-    result += `<div>${reply.replyer}</div>`;
-    result += `<div><span class="fs-5">${reply.text}</span></div>`;
-    result += `<div class="text-muted"><span class="small">${formatDate(reply.createdDate)}</span></div>`;
-    result += `</div>`;
-    result += `<div class="d-flex flex-column align-self-center">`;
-    result += `<div class="mb-2">`;
-    result += `<button class="btn btn-outline-danger btn-sm">삭제</button>`;
-    result += `</div>`;
-    result += `<div><button class="btn btn-outline-success btn-sm">수정</button></div>`;
-    result += `</div></div></div></div></div>`;
-  });
+      result += `</div>`;
+      result += `<div class="flex-grow-1 align-self-center">`;
+      result += `<div>${reply.replyer}</div>`;
+      result += `<div><span class="fs-5">${reply.text}</span></div>`;
+      result += `<div class="text-muted"><span class="small">${formatDate(reply.createdDate)}</span></div>`;
+      result += `</div>`;
+      result += `<div class="d-flex flex-column align-self-center">`;
+      result += `<div class="mb-2">`;
+      result += `<button class="btn btn-outline-danger btn-sm">삭제</button>`;
+      result += `</div>`;
+      result += `<div><button class="btn btn-outline-success btn-sm">수정</button></div>`;
+      result += `</div></div></div></div>`;
+    });
 
-  document.querySelector(".replyList").innerHTML = result;
+    document.querySelector(".replyList").innerHTML = result;
+
+    // replyCount
+    const replyCount = document.querySelector("h5 > .cnt");
+    replyCount.innerHTML = data.length;
+  });
 };
 
 // 댓글 리스트 생성
-axios.get(`/replies/board/${bno}`).then((res) => {
-  console.log(res.data);
+replyList();
 
-  let data = res.data;
-  replyList(data);
+// st
+replyForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const rno = form.rno.value;
+
+  if (rno) {
+    // 수정
+    console.log(form);
+    axios
+      .put(`/replies/${rno}`, form, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        alert("댓글 수정 완료");
+        replyForm.rno.value = "";
+        replyForm.replyer.value = "";
+        replyForm.text.value = "";
+
+        // todo 댓글 리스트 재 호출
+        replyList();
+      });
+  } else {
+    // todo 삽입
+    axios
+      .post("/replies/new", form, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+    replyList();
+  }
 });
+// end
 
 // 댓글의 버튼 입력 감지(삭제, 수정)
 document.querySelector(".replyList").addEventListener("click", (e) => {
@@ -73,14 +118,18 @@ document.querySelector(".replyList").addEventListener("click", (e) => {
     if (!confirm("정말로 삭제하시겠습니까?")) return;
 
     axios.delete(`/replies/${rno}`).then((res) => {
-      console.log(res.data);
       //삭제 후 댓글 리로드
     });
+    replyList();
   } else if (btn.classList.contains("btn-outline-success")) {
     // TODO: 수정할 댓글 작성하는 기능 구현
 
-    axios.put(`/replies/${rno}`).then((res) => {
-      console.log(res.data);
+    axios.get(`/replies/${rno}`).then((res) => {
+      const data = res.data;
+
+      replyForm.rno.value = data.rno;
+      replyForm.replyer.value = data.replyer;
+      replyForm.text.value = data.text;
     });
   }
 });
